f12aed137590ea4525635ff6b8fd440034f997ae
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-03-10 10:33:22 +0700

    add valid password check

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const form = document.getElementById("form");
+const password = document.getElementById("password");
+const passwordRule = document.querySelector(".password_rule");
+
+// return value from 1 to 3 present the nth rule are applied
+function checkValidPasword(str){
+    
+}
 
 form.addEventListener("submit", e => {
     e.preventDefault();
