31b6a51a6ca71a0596de735cbf622539df780c12
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-04-28 16:50:49 +0700

    Prettier

--- script.js
+++ script.js
@@ -3,31 +3,30 @@ const password = document.getElementById("password");
 const confirm = document.getElementById("confirm");
 
 const rules = {
-    length: v => v.length >= 8,
-    number: v => /[0-9]/.test(v),
-    character: v => /[A-Za-z]/.test(v),
-}
+  length: (v) => v.length >= 8,
+  number: (v) => /[0-9]/.test(v),
+  character: (v) => /[A-Za-z]/.test(v),
+};
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-}); 
-
-password.addEventListener("input", (e) => {
-    const value = password.value;
-
-    for (let rule in rules){
-        let element = document.getElementById(rule);
-        element.classList.toggle("valid", rules[rule](value));
-    }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 });
 
-function validatePassword(){
-    if (password.value !== confirm.value){
-        confirm.setCustomValidity("Password don not match");
-    }
-    else{
-        confirm.setCustomValidity("");
-    }
+password.addEventListener("input", (e) => {
+  const value = password.value;
+
+  for (let rule in rules) {
+    let element = document.getElementById(rule);
+    element.classList.toggle("valid", rules[rule](value));
+  }
+});
+
+function validatePassword() {
+  if (password.value !== confirm.value) {
+    confirm.setCustomValidity("Password don not match");
+  } else {
+    confirm.setCustomValidity("");
+  }
 }
 
 confirm.addEventListener("input", validatePassword);
